2b17dd069c6e971388f9e2ae42310d3bcee125c4
baiest <ballesteros.juan@correounivalle.edu.co>
2021-12-15 10:21:54 -0500

    Router configurado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,9 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
 import { SwiperModule } from 'swiper/angular';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +35,9 @@ import { SwiperModule } from 'swiper/angular';
     HeaderComponent,
     FooterComponent,
     BannerComponent,
+    PageNotFoundComponent,
+    ProductDetailComponent,
+    LayoutComponent,
   ],
   imports: [
   BrowserModule,
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './../product.model';
+import { ProductsService } from './../products.service';
 
 @Component({
   selector: 'app-products',
@@ -7,53 +8,13 @@ import { Product } from './../product.model';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
-  products: Product[] = [ {
-    id: '1',
-    image: 'assets/images/camiseta.webp',
-    title: 'Camiseta',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  },
-  {
-    id: '2',
-    image: 'assets/images/hoodie.webp',
-    title: 'Hoodie',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  },
-  {
-    id: '3',
-    image: 'assets/images/mug.webp',
-    title: 'Mug',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  },
-  {
-    id: '4',
-    image: 'assets/images/pin.webp',
-    title: 'Pin',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  },
-  {
-    id: '5',
-    image: 'assets/images/stickers1.webp',
-    title: 'Stickers',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  },
-  {
-    id: '6',
-    image: 'assets/images/stickers2.webp',
-    title: 'Stickers',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  }]
-
-  constructor() { }
+  products: Product[]
+  constructor(
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.products = this.productsService.getAllProducts()
   }
 
   clickProduct(id: any){
